3de26c115920b4241684c21eb039518176c0128d
Oleg Utkin <oleggator@users.noreply.github.com>
2017-04-25 16:11:08 +0300

    Remove example fields

--- src/MainView.h
+++ src/MainView.h
@@ -5,9 +5,7 @@
 
 class MainView : public QObject {
     Q_OBJECT
-public:
-    Q_INVOKABLE QDateTime getCurrentDateTime() const;
-
+        
 public slots:
     void log(const QString& msg) {
     	qDebug() << msg;
