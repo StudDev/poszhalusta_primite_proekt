d3fb86de77cc57d3fd71210d4b1239e4cbc4be4b
Oleg Utkin <oleggator@users.noreply.github.com>
2017-04-25 16:15:58 +0300

    Remove example lines

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,8 @@
 #include "MainView.h"
 
 int main(int argc, char *argv[]){
-  // QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
   QGuiApplication app(argc, argv);
-  // QQmlApplicationEngine engine;
-  // engine.load(QUrl(QLatin1String("qrc:/main.qml")));
-
+  
   QQuickView view;
   MainView mainView;
   view.rootContext()->setContextProperty("mainView", &mainView);
