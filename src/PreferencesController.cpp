891548b7f6b6ea50bfea6c7a7ff5bb1eb644e732
Oleg Utkin <oleg_utkin@me.com>
2017-05-08 19:51:41 +0300

    Add sync folder selector

--- src/PreferencesController.cpp
+++ src/PreferencesController.cpp
@@ -1,15 +1,47 @@
 #include "PreferencesController.h"
+#include <QDir>
+#include <QQmlContext>
+#include <QDebug>
 
-PreferencesController::PreferencesController() {
-    view.rootContext()->setContextProperty("controller", this);
-    const QSize windowSize = QSize(640, 480);
-    view.setMinimumSize(windowSize);
-    view.setMaximumSize(windowSize);
+PreferencesController::PreferencesController(QObject* parent)
+    : QObject(parent) {
+  if (_settings.allKeys().contains("paths/root"))
+    _root_path = QDir::cleanPath(_settings.value("paths/root").toString());
 
-    view.setSource(QUrl(QLatin1String("qrc:/PreferencesView.qml")));
-    view.show();
+  QDir rootFolder(_root_path);
+  if (!rootFolder.exists())
+    rootFolder.mkpath(".");
+
+  _settings.setValue("paths/root", _root_path);
+  
+
+  _view.rootContext()->setContextProperty("controller", this);
+  _view.rootContext()->setContextProperty("view", &_view);
+
+  const QSize windowSize = QSize(640, 480);
+  _view.setMinimumSize(windowSize);
+  _view.setMaximumSize(windowSize);
+
+  _view.setSource(QUrl(QLatin1String("qrc:/PreferencesView.qml")));
 }
 
-void PreferencesController::log(const QString& msg) {
-    qDebug() << msg;
+void PreferencesController::changeRoot(const QUrl& url) {
+  QDir root(url.path());
+  root.setFilter(QDir::AllDirs | QDir::NoSymLinks | QDir::NoDotAndDotDot);
+  _root_path = root.canonicalPath();
+  _settings.setValue("paths/root", _root_path);
+
+  QStringList allFolders;
+  recursiveAddFolder(allFolders, root.canonicalPath());
+
+  _settings.setValue("paths/watchingDirs", allFolders);
+}
+
+void PreferencesController::recursiveAddFolder(QStringList& foldersList, const QString& rootFolder) {
+  QDir root(rootFolder);
+  foldersList << root.canonicalPath();
+  qDebug() << root.canonicalPath() + " is added to config";
+
+  for (const QString& folderPath: root.entryList(QDir::AllDirs | QDir::NoSymLinks | QDir::NoDotAndDotDot))
+    recursiveAddFolder(foldersList, root.filePath(folderPath));
 }
