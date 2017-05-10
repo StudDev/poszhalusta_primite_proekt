1413ef4f4e645e494511c7c82d2b0da6db6fe8b8
Kirill-Semin <qseminq@gmail.com>
2017-05-10 05:18:19 +0300

    TODO: FWController

--- src/FileWatch.cpp
+++ src/FileWatch.cpp
@@ -10,40 +10,51 @@ FileWatch::FileWatch(QObject *parent) : QObject(parent) {
         local_errno_ = errno;
         emit FileWatchInitError();
     }
+
+    epoll_descriptor_ = epoll_create1(0);
+    if(-1 == epoll_descriptor_) {
+        local_errno_ = errno;
+        emit FileWatchInitError();
+    }
 }
 
 
 FileWatch::~FileWatch() {
     close(inotify_descriptor_);
+    close(epoll_descriptor_);
 }
 
 void FileWatch::AddDirectory(const QDir &arg) {
     if(!arg.exists()) {
-        qDebug() << "Directory doesn't exist: " << arg.canonicalPath();
+        qDebug() << "Directory doesn't exist: " << arg;
         emit FileWatchWrongArgument();
         return;
     }
+
     if(hash_by_directory_.contains(arg.canonicalPath())) {
-        qDebug() << "Adding contained directory: " << arg.canonicalPath();
+        qDebug() << "Adding already contained directory: " << arg;
         emit FileWatchDirectoryAlreadyAdded();
         return;
     }
-    int temp_watch_descriptor = inotify_add_watch(inotify_descriptor_, TransformQDirToChar(arg), WATCH_FLAGS_);
+
+    int temp_watch_descriptor = inotify_add_watch(inotify_descriptor_,
+                                                  arg.canonicalPath().toStdString().c_str(), WATCH_FLAGS_);
     if(-1 == temp_watch_descriptor) {
         local_errno_ = errno;
-        qDebug() << "Adding watch failed";
+        qDebug() << "Adding watch failed: unknown error";
         emit FileWatchError();
         return;
     }
+
     hash_by_directory_.insert(arg.canonicalPath(), temp_watch_descriptor);
     hash_by_descriptor_.insert(temp_watch_descriptor, arg.canonicalPath());
 }
 
 void FileWatch::RemoveDirectory(const QDir &arg) {
 
-    //check for existance argument directory in list of watched directories
+    //check for existence argument directory in list of watched directories
     if(!hash_by_directory_.contains(arg.canonicalPath())) {
-        qDebug() << "Removing directory doesn't exists: " << arg.canonicalPath();
+        qDebug() << "Removing directory doesn't exists: " << arg;
         emit FileWatchWrongArgument();
         return;
     }
@@ -69,9 +80,125 @@ void FileWatch::RemoveDirectory(const QDir &arg) {
 }
 
 void FileWatch::StartWatch() {
-    
+    epoll_event inotify_ev;
+    epoll_event console_ev;
+    epoll_event events[MAX_EPOLL_EVENTS];
+
+    inotify_ev.events = EPOLLIN;
+    console_ev.events = EPOLLIN;
+
+    inotify_ev.data.fd = inotify_descriptor_;
+    console_ev.data.fd = STDIN_FILENO;
+
+    if(-1 == epoll_ctl(epoll_descriptor_, EPOLL_CTL_ADD, inotify_descriptor_, &inotify_ev)) {
+        qDebug() << "epoll add error";
+        emit FileWatchError();
+    }
+    if(-1 == epoll_ctl(epoll_descriptor_, EPOLL_CTL_ADD, console_ev.data.fd, &console_ev)) {
+        qDebug() << "epoll add error";
+        emit FileWatchError();
+    }
+
+    qDebug() << "Listening for events";
+
+    bool exit_flag = false;
+    while(true) {
+        int events_available = epoll_wait(epoll_descriptor_, events, MAX_EPOLL_EVENTS, -1);
+        if(-1 == events_available) {
+            qDebug() << "epoll wait failed";
+            emit FileWatchError();
+        }
+        for(int i = 0; i < events_available; ++i) {
+            if(events[i].data.fd == STDIN_FILENO) {
+                char temp_symbol;
+                while(read(STDIN_FILENO, &temp_symbol, 1) > 0 && temp_symbol != '\n') {
+                    continue;
+                }
+                exit_flag = true;
+                qDebug() << "Listening finished";
+                break;
+            }
+
+            if(events[i].data.fd == inotify_descriptor_) {
+                HandleEvents();
+            }
+        }
+        if(exit_flag) {
+            break;
+        }
+    }
 }
 
-const char *FileWatch::TransformQDirToChar(const QDir &arg) const {
-    return arg.canonicalPath().toStdString().c_str();
+void FileWatch::HandleEvents() {
+    char buf[MAX_INOTIFY_EVENT_SIZE];
+    ssize_t read_len;
+
+    while(true) {
+        read_len = read(inotify_descriptor_, buf, sizeof(buf));
+
+        //eagain means "no data available" so we skip this error
+        if(-1 == read_len && errno != EAGAIN) {
+            qDebug() << "Inotify descriptor read failure";
+            emit FileWatchError();
+            return;
+        }
+
+        //we read the entire buffer
+        if(read_len <= 0) {
+            break;
+        }
+
+        const inotify_event* event;
+
+        void* unread_events_ptr;
+
+        //we can advance unread_events_ptr on event->len, because is happens only after the first iteration
+        //so at that moment we have initialized "event->len"
+        for(unread_events_ptr = buf; unread_events_ptr < buf + read_len;
+            unread_events_ptr += sizeof(inotify_event) + event->len) {
+
+            event = static_cast<inotify_event*>(unread_events_ptr);
+
+
+            //filter to avoid unix - temporary files beginning with "."
+            if(event->len) {
+                QString name = event->name;
+                QRegExp check_reg("^(?![.])(?!.*[-_.]$).+");
+                if(!check_reg.exactMatch(name))
+                    continue;
+            }
+
+
+            if(event->mask & IN_CREATE) {
+                qDebug() << "IN_CREATE: ";
+            }
+            if(event->mask & IN_DELETE) {
+                qDebug() << "IN_DELETE: ";
+            }
+            if(event->mask & IN_DELETE_SELF) {
+                qDebug() << "IN_DELETE_SELF: ";
+            }
+            if(event->mask & IN_MODIFY) {
+                qDebug() << "IN_MODIFY: ";
+            }
+            if(event->mask & IN_MOVE_SELF) {
+                qDebug() << "IN_MOVE_SELF: ";
+            }
+            if(event->mask & IN_MOVED_FROM) {
+                qDebug() << "IN_MOVED_FROM: ";
+            }
+            if(event->mask & IN_MOVED_TO) {
+                qDebug() << "IN_MOVED_TO: ";
+            }
+
+            qDebug() << hash_by_descriptor_.value(event->wd);
+
+            if(event->len) {
+                qDebug() << event->name;
+            }
+
+            qDebug() << "-------------------------------------------------------";
+        }
+    }
 }
+
--- src/FileWatch.h
+++ src/FileWatch.h
@@ -7,8 +7,9 @@
 #include <QtCore/QObject>
 
 #include <QString>
+#include <QRegExp>
 #include <QDir>
-#include <vector>
+#include <QQueue>
 
 //for storing   <directory, watch_descriptor> and
 //              <watch_descriptor, directory>
@@ -17,22 +18,22 @@
 //for inotify_init(..)
 #include <sys/inotify.h>
 
+//for using polls (blocking wait in watch)
+#include <sys/epoll.h>
+
 //for "close(inotify_descriptor)
 #include <unistd.h>
 
 //for using ernno (last error)
 #include <cerrno>
 
-//
-// Created by alting on 07.05.17.
-//
 
+//TODO: FSEvent signal
 class FileWatch : public QObject {
     Q_OBJECT
 public:
     explicit FileWatch(QObject *parent = nullptr);
     ~FileWatch();
-
     //void AddRecursiveDirectory(QDir& arg);
     void AddDirectory(const QDir& arg);
     //void AddFile(QDir& arg);
@@ -43,18 +44,27 @@ signals:
     void FileWatchError();
     void FileWatchWrongArgument();
     void FileWatchDirectoryAlreadyAdded();
+
 protected:
     const static auto WATCH_FLAGS_ = (IN_CREATE | IN_DELETE | IN_DELETE_SELF |
-                                      IN_MODIFY | IN_MOVE_SELF | IN_MOVE);
+                                      IN_MODIFY | IN_MOVE_SELF | IN_MOVE | IN_DONT_FOLLOW);
+    const static auto MAX_INOTIFY_EVENT_SIZE = sizeof(inotify_event) + NAME_MAX + 1;
+    const static int MAX_EPOLL_EVENTS = 10;
+
+    void HandleEvents();
+
+
+    //we use 2 hashtables to have fast access both by directory and filewatch descriptor
+    //this could be a single boost::bimap
     QHash<QString, int> hash_by_directory_;
     QHash<int, QString> hash_by_descriptor_;
+
+    //file descriptors, returned by inotify and epoll initialization functions
     int inotify_descriptor_;
+    int epoll_descriptor_;
     int local_errno_;
-private:
-    static const char* TransformQDirToChar(const QDir& arg) const;
-    static const unsigned MAX_EVENTS = 4096;
-    static const unsigned EVENT_SIZE = (sizeof(inotify_event));
-    static const unsigned EVENT_BUF_LEN = (MAX_EVENTS * (EVENT_SIZE + 16));
+
+
 };
 
 
